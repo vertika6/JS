2d36923601c9e1bc774995b4875d2dcd99241855
Vertika Gupta <69085792+vertika6@users.noreply.github.com>
2023-10-22 07:28:44 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+//datatypes
+
+//let name="work"
+
+//let age=18
+//let state;
+//number=> 2 to power 53
+//bigInnt
+//string=>""
+//boolean=> true/false
+//null=>standalone value
+//undefined=>
+//symbol=> unique
+
+
+//object
+
+//console.log(type of null);=> object
+//console.log(type of undefined); => undefined
