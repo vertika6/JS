570b93477962689f2a981e18d5950859fdbb5e4f
Vertika Gupta <69085792+vertika6@users.noreply.github.com>
2023-10-22 15:57:50 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,13 @@
+//console.log("2" >1)  => true           //conversion happens
+//console.log("02" >1)   => true
+
+//console.log(null>0)    => false       //  >,<,>=,<= convert null to 0
+//console.log(null==0)    => false       //  == work different
+//console.log(null>=0)     => true
+
+
+//console.log(undefined<0)   =>false
+//console.log(undefined>0)   ->false
+//console.log(undefined==0)    =>fasle
+
+//console.log("2"===2)       =>false           //strict check
