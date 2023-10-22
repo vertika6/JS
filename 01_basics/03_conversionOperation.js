fecb8f2f76087ae325c5418cd81ca747a6431fc0
Vertika Gupta <69085792+vertika6@users.noreply.github.com>
2023-10-22 07:50:48 +0000

    operation on coversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,17 @@
+//conversion to number
+
+//"33" =>33
+//"33qaabc"=>NaN
+//null=> 0
+//undefined=> NaN
+
+//conversion to boolean
+
+//1=> true
+//0=> false
+//""=>false
+//"verti"=> true
+
+//conversion to string
+
+//33=> 33
