1a837b4367ef4c3d916b7b69a8e99c179b3b1220
Vertika Gupta <69085792+vertika6@users.noreply.github.com>
2023-10-24 19:16:05 +0000

    number and math

---
+++ 01_basics/06_numbers.js
@@ -0,0 +1,33 @@
+//const score=400                   =>400
+// const bal= new Number(100)        =>100
+
+//console.log(bal.toString().leghth)   =>3
+
+//cosole.log(bal.toFixed(2))           =>100.00
+
+//const otherNo=123.45
+
+//console.log(otherNo.toPrecision(3))    =>123
+
+// precision digit starts count from start of number only  after decimal place
+
+//const hundrreds=1000000
+//console.log(hundreds.toLocaleString('en-IN'))         //10,00,000
+
+//Math
+
+//console.log(Math)                                =>Object[Math] {}
+
+//console.log(Math.abs(-4))                         =>4
+
+//console.log(Math.random)                       ->anything btwn 0 and 1
+
+//to get btwn 1 and 10           (Math.random()*10) +1       to avoid gettig 0 as o/p we add 1
+
+//to get random betwn max and min
+
+//Math.random()*(max-min+1)+min
+
+//to get round off value use floor
+
+//Math.floor(Math.random()*(max-min+1))+min)
